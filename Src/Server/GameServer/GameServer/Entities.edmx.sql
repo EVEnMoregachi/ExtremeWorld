0a22c67cb9e37d4b94ce65da5b380e6693c2556f
EVEnMore <143513377+EVEnMoregachi@users.noreply.github.com>
2024-10-03 23:57:23 +0800

    24.10.3 准备完毕
    完成了开始前的准备
    对应课程1.0完成

--- Src/Server/GameServer/GameServer/Entities.edmx.sql
+++ Src/Server/GameServer/GameServer/Entities.edmx.sql
@@ -2,8 +2,8 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 09/07/2018 00:02:53
--- Generated from EDMX file: D:\Projects\ExtremeWorld\project-z\Src\Server\GameServer\GameServer\Entities.edmx
+-- Date Created: 10/03/2024 23:46:26
+-- Generated from EDMX file: F:\Unity\Projects\24.10.3ExtremeWorld\Src\Server\GameServer\GameServer\Entities.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
